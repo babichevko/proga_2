d1f9e1fbbc2545a84c600769c697432b93be5ad2
babichevko <babichev.ko@phystech.edu>
2021-10-21 22:55:05 +0300

    добавил модуль с описанием всех базовых функций а также сгруппированных из них функций обработки событий мыши handler, счётчика tick, инициализации init; осталась единственная проблема: почему-то основная программа не может найти массивы balls, polygons в модуле

--- поймайшарикчереpмодули.py
+++ поймайшарикчереpмодули.py
@@ -1,1 +1,79 @@
+import pygame
+from pygame.draw import *
 
+import model
+import colors
+
+model.init()
+
+"Функция отрисовки нового квадрата"
+def draw_polygon(color, x2, y2, r2, width):
+    polygon(screen, color, [(x2, y2), (x2 + r2, y2), (x2 + r2, y2 + r2), (x2, y2 + r2)], width)
+
+"Функция отрисовки нового шара"
+def draw_ball(color, x, y, r):
+    circle(screen, color, (x, y), r)
+
+#Функция вывода счёта
+def schet():
+    schetchik = pygame.font.Font(None, 100)
+    textsurface = schetchik.render(str(model.chislo_ochkov), False, colors.RED)
+    screen.blit(textsurface, (100, 100))
+
+def zavershenie_igry():
+    clock.tick(FPS)
+    cenok = pygame.font.Font(None, 200)
+
+    textsurface = cenok.render('Game Over', False, colors.RED)
+    screen.blit(textsurface, (200, 300))
+    cenok2 = pygame.font.Font(None, 200)
+
+    textsurface2 = cenok2.render('Ваши очки:', False, colors.GREEN)
+    screen.blit(textsurface2, (200, 450))
+    cenok3 = pygame.font.Font(None, 200)
+
+    textsurface3 = cenok3.render(str(model.chislo_ochkov), False, colors.BLUE)
+    screen.blit(textsurface3, (500, 600))
+    pygame.display.update()
+
+
+pygame.init()
+
+FPS = 30
+screen = pygame.display.set_mode((1200, 900))
+
+
+clock = pygame.time.Clock()
+finished = False
+pygame.display.update()
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            #Обработка событий от мыши
+            model.handler(event.button, event.pos)
+
+    #вызов обсчёта модели
+    model.tick()
+
+    screen.fill(colors.BLACK)
+
+    # Рисуем шары и квадраты
+    for i in range(len(model.balls)):
+        color, x, y, r = model.balls[i]
+        draw_ball(color, x, y, r)
+
+    for i in range(len(model.polygons)):
+        color, x2, y2, r2, width = model.polygons[i]
+        draw_polygon(color, x2, y2, r2, width)
+
+    # Функция вывода счёта
+    schet()
+
+    pygame.display.update()
+
+
+pygame.quit()
